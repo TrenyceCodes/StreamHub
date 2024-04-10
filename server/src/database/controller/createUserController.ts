34d719dfa5ad7770b990fe5d9045dc0d7882222f
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-04-10 11:02:52 -0400

    Registration Validation is finished

--- server/src/database/controller/createUserController.ts
+++ server/src/database/controller/createUserController.ts
@@ -1,26 +1,56 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 import { Users } from "../models/Users";
 import bcrypt from "bcrypt";
+import validator from "validator";
+import PasswordValidator from "password-validator";
+
+function passwordValidator(password: string) {
+    const validation = new PasswordValidator();
+    return validation.min(2).max(8).validate(password);
+}
+
+async function hashPassword(password: string): Promise<String> {
+    const saltRounds = 10;
+
+    try {
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+        return hashPassword;
+    } catch (error) {
+        throw new Error("There was an issue hashing your password: " + error);
+    }
+}
 
 export const createUserController = async (request: Request, response: Response) => {
     try {
-        const saltRounds = 10;
-        bcrypt.hash(request.body.password, saltRounds, async function(error, hashedPassword) {
-            const user = new Users({ 
-                username: request.body.username,
-                emailaddress: request.body.emailaddress,
-                password: hashedPassword,
-                isLoggedIn: request.body.isLoggedIn
-            });
+        const {username, emailaddress, password, isLoggedIn} = request.body;
+        const currentUser = await Users.exists({username});
 
-            if (error) {
-                return response.status(400).json({message: "There was an issue hashing your password", error: error});
-            }
+        if (validator.isEmpty(username) || validator.isEmpty(emailaddress) || validator.isEmpty(password)) {
+            return response.status(400).json({message: "User input field should not be empty"});
+        }
 
-            await user.save();
-            return response.status(200).json({message: "User has been successfully created", data: user});
+        if (currentUser) {
+            return response.status(400).json({message: "username or emailaddress already in use"});
+        }
+
+        if (!validator.isEmail(emailaddress)) {
+            return response.status(400).json({message: "User email address is not valid", data: {emailaddress}});
+        }
+
+        if (passwordValidator(password)) {
+            return response.status(200).json({message: ""})
+        } 
+        
+        const hashedPassword = await hashPassword(password);
+        const user = new Users({ 
+            username: username,
+            emailaddress: emailaddress,
+            password: hashedPassword,
+            isLoggedIn: isLoggedIn
         });
 
+        await user.save();
+        return response.status(200).json({message: "User has been successfully created", data: user});
     } catch (error) {
         return response.status(400).json({message: "Error creating user", error: error});
     }
